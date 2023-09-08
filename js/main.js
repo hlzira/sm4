f7e62ef3cab26f412e9a43b275f505e9c460fb1a
hlzira <ralinaramilevna@mail.ru>
2023-09-08 10:14:08 +0300

    sm4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 price = prompt ("Введите стоимость товара")
 cash = prompt ("Введите количество денег клиента")
  if(price==cash){
-  alert('Покупка совершена')
+  textarea.textContent = ('Покупка совершена')
  } else {
-  if(price>cash){
+  if(price>=cash){
     ost = cash - price;
-    alert('Для покупки не хватает ' + -ost + ' р.')
+    textarea.textContent = ('Для покупки не хватает ' + -ost + ' р.')
   }else{
     sdacha = price - cash;
-    alert('Покупка совершена. Сдача ' + -sdacha + 'р.')
+    textarea.textContent = ('Покупка совершена. Сдача ' + -sdacha + 'р.')
   }
  }
+
+textarea.style.background = 'darkblue'
+textarea.style.height = '40px'
+textarea.style.width = '250px'
+textarea.style.padding = '20px'
+textarea.style.borderRadius = '20px'
+textarea.style.color = 'white'
+textarea.style.fontSize = '18px'
+textarea.style.fontFamily = 'Poppins'
+
+textarea = document.getElementById('textarea')
